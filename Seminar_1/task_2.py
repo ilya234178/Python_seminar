e3606f5f1c0386452beff8e95c9aa5e34e2d5062
Ilya <veruhanov00@mail.ru>
2022-09-28 13:54:27 +0300

    add task2

--- Seminar_1/task_2.py
+++ Seminar_1/task_2.py
@@ -1,3 +1,18 @@
 # напишите программу которая на вход принимает 5 чисел и находит максимальное из них
 
 
+num1 = int(input('Введите число 1 :'))
+num2 = int(input('Введите число 2 :'))
+num3 = int(input('Введите число 3 :'))
+num4 = int(input('Введите число 4 :'))
+num5 = int(input('Введите число 5 :'))
+
+arr = [num1,num2,num3,num4,num5]
+numMax = num1
+
+for i in arr :
+ if numMax < i :
+    numMax = i
+print(f'Максимальное число : {numMax}')        
+
+
