62f9be782b7f4103cc48893d354f87f9d0d03814
Ilya <veruhanov00@mail.ru>
2022-09-28 15:42:04 +0300

    add task4

--- Seminar_1/task_4.py
+++ Seminar_1/task_4.py
@@ -1,4 +1,12 @@
 # напишите программу которая будет принимать на вход дробь и показывать первую цифру дробной части числа
 
 
+import math
+
+
 n = float(input('Введите дробное число : '))
+num_result = n*10
+if num_result%10 == 0:
+    print("no")
+else:
+    print(math.floor(num_result)%10)
