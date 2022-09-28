177e8862ee76e7e06dd2246cd8562be85d747acc
Ilya <veruhanov00@mail.ru>
2022-09-28 14:18:23 +0300

    add 2 answer task 1

--- Seminar_1/task_1.py
+++ Seminar_1/task_1.py
@@ -3,9 +3,15 @@
 
 a = int(input('Введите число 1 :'))
 b = int(input('Введите число 2 :'))
-if a*a == b:
-    print(f'{a},{b} -> да')
-elif b*b == a:
+# if a*a == b:
+#     print(f'{a},{b} -> да')
+# elif b*b == a:
+#      print(f'{a},{b} -> да')
+# else:
+#      print(f'{a},{b} -> нет')
+
+
+if a**2 == b or b**2 == a : 
      print(f'{a},{b} -> да')
 else:
      print(f'{a},{b} -> нет')
